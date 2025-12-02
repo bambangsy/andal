83e4352ed850feb2f53cfcc058225f30c0245887
bambangsy <bambangsy28@gmail.com>
2025-12-02 07:26:10 +0700

    addchange password

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Support\Facades\Hash;
 
 class EditUser extends EditRecord
 {
@@ -13,6 +15,29 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('changePassword')
+                ->label('Change Password')
+                ->icon('heroicon-m-key')
+                ->color('warning')
+                ->form([
+                    TextInput::make('password')
+                        ->label('New Password')
+                        ->password()
+                        ->required()
+                        ->minLength(8)
+                        ->revealable(),
+                    TextInput::make('password_confirmation')
+                        ->label('Confirm Password')
+                        ->password()
+                        ->same('password')
+                        ->required()
+                        ->revealable(),
+                ])
+                ->action(function (array $data): void {
+                    $this->record->update([
+                        'password' => Hash::make($data['password']),
+                    ]);
+                }),
             Actions\DeleteAction::make(),
         ];
     }
